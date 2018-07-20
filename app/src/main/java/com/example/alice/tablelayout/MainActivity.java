c0b15e6a6d397372f0d6cdc958b01b5eef550c86
Alice2458 <18845788065@163.com>
2018-07-20 15:17:12 +0800

    lpp

--- app/src/main/java/com/example/alice/tablelayout/MainActivity.java
+++ app/src/main/java/com/example/alice/tablelayout/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.alice.tablelayout;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private String[][] titleDate = new String[][]{
+            {"ID","Name","Email","Address"},
+            {"itlty","李铁英","lty106@163.com","哈尔滨南岗区"},
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        TableLayout layout = new TableLayout(this);
+        TableLayout.LayoutParams layoutParams = new TableLayout.LayoutParams(
+                ViewGroup.LayoutParams.FILL_PARENT,
+                ViewGroup.LayoutParams.FILL_PARENT
+        );
+        layout.setBackgroundResource(R.drawable.bg2);
+        for (int x = 0; x < this.titleDate.length; x++){
+            TableRow row = new TableRow(this);
+            for (int y = 0; y < this.titleDate[x].length; y++){
+                TextView text = new TextView(this);
+                text.setText(this.titleDate[x][y]);
+                row.addView(text, y);
+            }
+            layout.addView(row);
+        }
+        setContentView(layout, layoutParams);
     }
 }
